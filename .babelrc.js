a9518e3975c1c952af8e4b4aff3365e11a583f37
Your Name <Burt_Harris@hotmail.com>
2020-03-13 19:31:32 -0700

    Add typescript dev support
    Disable strict checking during port
    
    enable typescript checkJs

--- .babelrc.js
+++ .babelrc.js
@@ -3,10 +3,14 @@ const isBrowser = process.env.BABEL_ENV === 'browser'
 const envOptions = isBrowser
   ? { modules: false, useBuiltIns: false }
   : { targets: { node: '6.5' }, useBuiltIns: false }
-const presets = [['@babel/env', envOptions]]
+const presets = [
+  ['@babel/env', envOptions],
+  '@babel/preset-typescript'
+]
 
 const plugins = [
   '@babel/plugin-proposal-class-properties',
+  '@babel/proposal-object-rest-spread',
   ['babel-plugin-trace', { strip: true }]
 ]
 if (isBrowser) plugins.push('@babel/plugin-transform-runtime')
